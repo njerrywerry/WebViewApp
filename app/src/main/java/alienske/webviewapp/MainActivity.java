62459a15a5b02f299ce364ad59e39dbdc13c4bfa
Sylvia <sylviakieha@gmail.com>
2015-07-26 21:51:40 +0300

    overrid my browser to open links in app

--- app/src/main/java/alienske/webviewapp/MainActivity.java
+++ app/src/main/java/alienske/webviewapp/MainActivity.java
@@ -3,6 +3,7 @@ package alienske.webviewapp;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,11 +13,18 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String url = "http://instagram.com/";
+//        String url = "http://instagram.com/";
         WebView view = (WebView) this.findViewById(R.id.webView);
         view.getSettings().setJavaScriptEnabled(true);
-        view.loadUrl(url);
+        view.setWebViewClient(new MyBrowser());
+        view.loadUrl("https://web.whatsapp.com");
     }
-
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
 
 }
